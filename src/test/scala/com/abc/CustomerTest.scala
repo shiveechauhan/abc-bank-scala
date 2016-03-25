f03e1dea13011c93cb30cfc96f4e9420d55d4649
shiveechauhan <shivee.chauhan@gmail.com>
2016-03-25 16:15:32 -0400

    Update CustomerTest.scala

--- src/test/scala/com/abc/CustomerTest.scala
+++ src/test/scala/com/abc/CustomerTest.scala
@@ -26,10 +26,47 @@ class CustomerTest extends FlatSpec with Matchers {
     oscar.openAccount(new Account(Account.CHECKING))
     oscar.numberOfAccounts should be(2)
   }
+  // Test to check that one customer can have only one of a particular account type
+  
+ 
+   it should "throw IllegalArgumentException This account type already exist. Please provide a new Account Type" in {
+    val oscar: Customer = new Customer("Oscar").openAccount(new Account(Account.SAVINGS))
+    oscar.openAccount(new Account(Account.SAVINGS))
+  }
+  
+   it should "throw IllegalArgumentException One of the accounts does not exist" in {
+    val checkingAccount: Account = new Account(Account.CHECKING)
+    val bob: Customer = new Customer("Bob").openAccount(checkingAccount)
+    bob.transferAmount(300,Account.SAVINGS,Account.CHECKING)
+  }
+  
+  it should "throw IllegalArgumentException Please provide two seperate account types" in {
+    val checkingAccount: Account = new Account(Account.CHECKING)
+    val bob: Customer = new Customer("Bob").openAccount(checkingAccount)
+    bob.transferAmount(300,Account.CHECKING,Account.CHECKING)
+  }
+  // Test to transfer amount between two accounts for a customer
+  
+   it should "transferAmount" in {
+    val checkingAccount: Account = new Account(Account.CHECKING)
+    val savingsAccount: Account = new Account(Account.SAVINGS)
+    val shivee: Customer = new Customer("Shivee").openAccount(checkingAccount).openAccount(savingsAccount)
+    checkingAccount.deposit(1000.0)
+    savingsAccount.deposit(4000.0)
+    shivee.transferAmount(300,Account.SAVINGS,Account.CHECKING)
+    shivee.getStatement should be("Statement for Shivee\n" +
+      "\nChecking Account\n  deposit $1000.00\nTotal $1000.00\n" +
+      "\nSavings Account\n  deposit $4000.00 \nTotal $4000.00\n" +
+      "\nChecking Account\n  withdrawal $300.00 \nTotal $700.00\n" +
+      "\nSavings Account\n  deposit $300.00 \nTotal $4300.00\n" +
+      "\nTotal In All Accounts $5000.00")
+  }
 
   ignore should "testThreeAcounts" in {
     val oscar: Customer = new Customer("Oscar").openAccount(new Account(Account.SAVINGS))
     oscar.openAccount(new Account(Account.CHECKING))
     oscar.numberOfAccounts should be(3)
   }
+  
+  
 }

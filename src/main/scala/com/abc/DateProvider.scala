2d15efb2565f21d930bea8889eb8cafad572148d
shiveechauhan <shivee.chauhan@gmail.com>
2016-03-25 16:18:08 -0400

    Update DateProvider.scala

--- src/main/scala/com/abc/DateProvider.scala
+++ src/main/scala/com/abc/DateProvider.scala
@@ -16,5 +16,10 @@ class DateProvider {
   def now: Date = {
     return Calendar.getInstance.getTime
   }
+  // This function returns the day of the year to calculate the number of days passed from the start of the year till current date
+    def dayOfYear: Int = {
+      Calendar calendar = Calendar.getInstance();
+     return calendar.get(Calendar.DAY_OF_YEAR)
+  }
 }
 

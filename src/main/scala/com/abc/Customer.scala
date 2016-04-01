774eba628435f904714602bcf21d8a27ee1b889f
shiveechauhan <shivee.chauhan@gmail.com>
2016-04-01 19:14:48 -0400

    Update Customer.scala

--- src/main/scala/com/abc/Customer.scala
+++ src/main/scala/com/abc/Customer.scala
@@ -3,27 +3,26 @@ package com.abc
 import scala.collection.mutable.ListBuffer
 
 class Customer(val name: String, var accounts: ListBuffer[Account] = ListBuffer()) {
-  
+
   // Function to check if a particular type of account exists
-  private def accountExist(var accType: Int): Boolean ={ 
-    val flag: Boolean = false
-      for(a <- accounts){
-        if(a.accountType == accType)
-           flag  = flag || true
-      }
-      return flag
+  private def accountExist(accType: Int): Boolean = {
+    var flag: Boolean = false
+    for (a <- accounts) {
+      if (a.accountType == accType)
+        flag = flag || true
     }
-  
-// Since the problem statetemt does not specify the limit on number of accounts of the same type a customer can open, an easier solution
-// to solve the #1 will be to limit the no. of accounts to 1 of each type for each customer. The test case has also been changed to
-// reflect this constraint. However, in a different aproach, we can introduce the concept of account IDs and set a counter to increment
-// the account ID as a new account of the same type is opened.
+    return flag
+  }
+
+  // Since the problem statetemt does not specify the limit on number of accounts of the same type a customer can open, an easier solution
+  // to solve the #1 will be to limit the no. of accounts to 1 of each type for each customer. The test case has also been changed to
+  // reflect this constraint. However, in a different aproach, we can introduce the concept of account IDs and set a counter to increment
+  // the account ID as a new account of the same type is opened.
 
   def openAccount(account: Account): Customer = {
-      if(accountExist(account.accountType))
-        {
-          throw new IllegalArgumentException("This account type already exist. Please provide a new Account Type")
-        }
+    if (accountExist(account.accountType)) {
+      throw new IllegalArgumentException("This account type already exist. Please provide a new Account Type")
+    }
     accounts += account
     this
   }
@@ -33,8 +32,8 @@ class Customer(val name: String, var accounts: ListBuffer[Account] = ListBuffer(
   def totalInterestEarned: Double = accounts.map(_.interestEarned).sum
 
   /**
-   * This method gets a statement
-   */
+    * This method gets a statement
+    */
   def getStatement: String = {
     //JIRA-123 Change by Joe Bloggs 29/7/1988 start
     var statement: String = null //reset statement to null here
@@ -61,34 +60,33 @@ class Customer(val name: String, var accounts: ListBuffer[Account] = ListBuffer(
     accountType + transactionSummary + totalSummary
   }
 
-  private def withdrawalOrDepositText(t: Transaction) =
+  private def withdrawalOrDepositText(t: Transaction) = {
     t.amount match {
       case a if a < 0 => "withdrawal"
       case a if a > 0 => "deposit"
       case _ => "N/A"
     }
+  }
 
   private def toDollars(number: Double): String = f"$$$number%.2f"
-  
-   def transferAmount(amount: Double,val fromAccountType: Int,val toAccountType: Int ) {
-    if (fromAccountType == toAccountType)
-    {
+
+  def transferAmount(amount: Double, fromAccountType: Int, toAccountType: Int)
+  {
+    if (fromAccountType == toAccountType) {
       throw new IllegalArgumentException("Please provide two seperate account types")
     }
-    else
-    {
-          if(!accountExist(fromAccountType) || !accountExist(toAccountType))
-          {
-            throw new IllegalArgumentException("One of the accounts does not exist")
-          }
-          else
-          {
-            val accountFrom: Account = new Account(fromAccountType)
-                accountFrom.withdraw(amount)
-            val accountTo: Account = new Account(toAccountType)
-            accountTo.deposit(amount)
-          }
-    } 
+    else {
+      if (!accountExist(fromAccountType) || !accountExist(toAccountType)) {
+        throw new IllegalArgumentException("One of the accounts does not exist")
+      }
+      else {
+        val accountFrom: Account = new Account(fromAccountType)
+        accountFrom.withdraw(amount)
+        val accountTo: Account = new Account(toAccountType)
+        accountTo.deposit(amount)
+      }
+    }
   }
+
 }
 

51bbc05787e70fbd0d23d8182020324cbcfb94c0
shiveechauhan <shivee.chauhan@gmail.com>
2016-04-01 19:12:45 -0400

    Update Account.scala

--- src/main/scala/com/abc/Account.scala
+++ src/main/scala/com/abc/Account.scala
@@ -1,6 +1,8 @@
 package com.abc
 
 import scala.collection.mutable.ListBuffer
+import java.util.Date
+import java.util.Calendar
 
 object Account {
   final val CHECKING: Int = 0
@@ -16,49 +18,52 @@ class Account(val accountType: Int, var transactions: ListBuffer[Transaction] = 
     else
       transactions += Transaction(amount)
   }
-// Refactored the withdrawal function to check if the available balance is enough to perform the withdrawal 
+  // Refactored the withdrawal function to check if the available balance is enough to perform the withdrawal
   def withdraw(amount: Double) {
     val totalBalance: Double = sumTransactions()
     if(totalBalance < amount)
       throw new IllegalArgumentException("You dont have enough balance for the withdrawal")
     else
     {
-    if (amount <= 0)
-      throw new IllegalArgumentException("amount must be greater than zero")
-    else
-      transactions += Transaction(-amount)
+      if (amount <= 0)
+        throw new IllegalArgumentException("amount must be greater than zero")
+      else
+        transactions += Transaction(-amount)
     }
   }
-  
-  // Check if there was a withdrawal in the past 10 days. I start traversing the ListBuffer Transactions in reverse order to 
-  // get the first instance of withdrawal and return true if it was less than equal to 10 days.
-  
-  private def pastTenDaysWithdrawal: Boolean ={
-     var today: Date = DateProvider.getInstance.now
-     for( t <- transactions.reverseIterator){
-       if(t.amount < 0 ){
-         var DateDiff: Int  = (today - t.transactionDate)
-         if(DateDiff <= 10 )
-         return true
-       }
-       return false
-     }
-  } 
 
-//The function has been changed to calculate the interest rate accrued daily #3. 
+  // Check if there was a withdrawal in the past 10 days. I start traversing the ListBuffer Transactions in reverse order to
+  // get the first instance of withdrawal and return true if it was less than equal to 10 days.
+
+  private def pastTenDaysWithdrawal: Boolean ={
+    var today: Date = DateProvider.getInstance.now
+    val reverselist: ListBuffer[Transaction] = transactions.reverse
+    var t: Transaction = null
+    var flag:Boolean = false
+    for( t <- reverselist){
+      if(t.amount < 0 ){
+        var DateDiff: Int  = today.compareTo(t.transactionDate)
+        if(DateDiff <= 10 )
+          flag = true
+      }
+    }
+    return flag
+  }
+
+  //The function has been changed to calculate the interest rate accrued daily #3.
   def interestEarned: Double = {
-    var numDays: Int = DateProvider.dayOfYear()
+    var numDays: Int = DateProvider.dayOfYear
     val amount: Double = sumTransactions()
     accountType match {
       case Account.SAVINGS =>
         if (amount <= 1000) amount * Math.pow((1+ 0.001/365),numDays)
         else 1 + (amount - 1000) * Math.pow((1+ 0.002/365),numDays)
       case Account.MAXI_SAVINGS =>
-     //Adding code to check if there was a withdrawal in past 10 days, and set the interest rate accordingly per #2
-          if(pastTenDaysWithdrawal)
-             amount * Math.pow((1+ 0.001/365),numDays)
-          else
-            amount * Math.pow((1+ 0.05/365),numDays)
+        //Adding code to check if there was a withdrawal in past 10 days, and set the interest rate accordingly per #2
+        if(pastTenDaysWithdrawal)
+          amount * Math.pow((1+ 0.001/365),numDays)
+        else
+          amount * Math.pow((1+ 0.05/365),numDays)
       case _ =>
         amount * Math.pow((1+ 0.001/365),numDays)
     }
